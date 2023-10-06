be8e9cfb0027af75591486e6fd1beb2e713afd99
Vikas Maurya <vikaskumarmaurya765@gmail.com>
2023-10-06 11:43:33 +0530

    first commit

---
+++ src/component/HeaderTop.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './headerTop.css'
+
+const HeaderTop = () => {
+    return <div className='Header_Top'>
+
+<div className='social_media_icon'>
+    <img src='./Assest/Facebook.png'></img>
+    <img src='./Assest/Twitter.png'></img>
+    <img src='./Assest/Google-plus.png'></img>
+    <img src='./Assest/Instagram.png'></img>
+    
+    
+   
+    
+
+</div>
+<div className='nav_tabs'>
+<div className='nav_tabs-tab'>
+<img src='./Assest/Login.png'></img>
+<div className=''>Login</div>
+
+
+</div>
+<div className='border'></div>
+<div className='nav_tabs-tab'>
+<img src='./Assest/Register.png'></img>
+<div>Register Now</div>
+
+
+</div>
+<div className='border'></div>
+<div className='nav_tabs-tab'>
+<img src='./Assest/Become-Member.png'></img>
+<div>Become Member</div>
+
+
+</div>
+</div>
+
+    </div>;
+}
+
+export default HeaderTop;
